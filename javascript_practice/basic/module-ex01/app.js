21e6a1a43e72a6ebc1036fee5e951ed4cadf089c
dhcho <oop6413@gmail.com>
2021-07-12 16:29:30 +0900

    2021.07.12 Javascript module, async practice

---
+++ javascript_practice/basic/module-ex01/app.js
@@ -0,0 +1,16 @@
+/* app module */
+const App = function() {
+    const app = {};
+    app.textContent = "Hello World";
+
+    return app;
+}
+
+// module.exports = function() {
+//     const app = {};
+//     app.textContent = "Hello World";
+
+//     return app;
+// }
+
+module.exports = App;
