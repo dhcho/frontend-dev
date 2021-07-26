1eefe84f62f5aea1659b4dfe1728c1e52c152f78
dhcho <oop6413@gmail.com>
2021-07-26 16:25:45 +0900

    2021.07.26 javascript class component practice

---
+++ javascript_practice/es6/ex08.02.mjs
@@ -0,0 +1,15 @@
+/**
+ * unnamed export(default)
+ * 
+ * 이름 없는 객체 하나를 exports 하는것이다.
+ * module.exports(CommonJS)
+ */
+
+export default {
+    add: function(a, b) {
+        return a + b;
+},
+    subtract: function(a, b) {
+        return a - b;
+    }
+}
